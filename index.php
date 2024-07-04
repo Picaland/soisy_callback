26d9edc71055c1ed232b13c5124839f5a6df5e15
alfiopiccione <alfio.piccione@gmail.com>
2024-07-04 08:43:11 +0200

    update workflow 1.0.2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Plugin Name: Soisy callback
  * Description: Soisy callback (unofficial) plugin for WooCommerce (change order status (completed or cancelled) via REST API - callback url: https://example.com/wp-json/api/v1/soisy_callback/ callback)
  *
- * Version: 1.0.1
+ * Version: 1.0.2
  *
  * Author: alfiopiccione <alfio.piccione@gmail.com>
  * Author URI: https://alfiopiccione.com/
